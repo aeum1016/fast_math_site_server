51d77c54b386cfcec704a3ce82273888061af57b
aeum1016 <aeum1016@gmail.com>
2022-03-19 22:57:21 -0400

    Attempt now has username

--- models/attempt.js
+++ models/attempt.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const attemptSchema = mongoose.Schema({
+  username: { type: String, required: true },
   email: { type: String, required: true },
   operation: { type: String, required: true },
   max: { type: Number, required: true },
